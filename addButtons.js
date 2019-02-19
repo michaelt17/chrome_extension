cc063cfceec0570a976aae35147c1c942592880f
michaelt17 <michaeltamkin2020@u.northwestern.edu>
2019-02-19 17:20:08 -0600

    added plus and minus buttons

--- addButtons.js
+++ addButtons.js
@@ -1,11 +1,40 @@
 (function() {
   let h2List = document.getElementsByClassName('doc-title');
-  let button = document.createElement('button');
-  button.innerHTML = "hello";
+  let button = document.createElement('BUTTON');
+  let buttonNeg = document.createElement('BUTTON');
+
+  button.type = "button";
+  button.classList.add("positive-button");
+
+  buttonNeg.type = "button";
+  buttonNeg.classList.add("negative-button");
+
+  // button.syle.width = "50px";
+  // button.syle.height = "50px";
+  // button.value = "hatelife";
   console.log(h2List.length);
   for (var i = 0; i < h2List.length; i++){
+    tempNode = button.cloneNode();
+    tempNode.innerHTML = '+';
+    tempNode.style.backgroundColor = "lightgreen";
+    tempNode.style.borderRadius = "50%";
+    tempNode.style.width = "50px";
+    tempNode.style.height = "50px";
+    tempNode.style.marginRight = "10px";
+    tempNode.style.float = "right";
+
+    tempNodeNeg = buttonNeg.cloneNode();
+    tempNodeNeg.innerHTML = '+';
+    tempNodeNeg.style.backgroundColor = "red";
+    tempNodeNeg.style.borderRadius = "50%";
+    tempNodeNeg.style.width = "50px";
+    tempNodeNeg.style.height = "50px";
+    tempNodeNeg.style.float = "right";
+
+    h2List[i].appendChild(tempNode);
+    h2List[i].appendChild(tempNodeNeg);
     console.log(h2List[i].innerHTML);
-    h2List[i].appendChild(button);
+    // h2List[i].innerHTML += button;
   }
 
 })();
--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 let changeColor = document.getElementById('changeColor');
 let addButtons = document.getElementById('addButtons');
 
+let buttonsAdded = false;
+
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
@@ -18,11 +20,15 @@ changeColor.onclick = function(element) {
 addButtons.onclick = function(element) {
   chrome.extension.getBackgroundPage().console.log(':)');
 
+  if (!buttonsAdded){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
           {file: 'addButtons.js'});
     });
+    buttonsAdded = true;
+  }
+
 
 
     // bigH1.appendChild(button);
